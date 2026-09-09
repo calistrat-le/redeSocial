8ee8ee289db7538bcf340332c9380db06c04b051
calistrat-le <calistrat.coc@gmail.com>
2026-09-09 11:03:43 -0300

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+const LikeBtn = document.querySelector(".like-btn"); 
+const postMedia = document.querySelector(".post-media");
+if (!LikeBtn) return;
+
+const likesCountSpan = LikeBtn.querySelector(".likes-count");
+const bookmarkBtn = document.querySelector(".bookmark-btn");
+let isliked = false;
+let baseLikes = 0;
+}
+)
